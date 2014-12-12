44616f12ffed4ca64f33f2249cebae2078de1638
Manuel Alejandro de Brito Fontes <aledbf@gmail.com>
2014-12-12 03:00:07 -0300

    fix(doc): urandom reference

--- launch.sh
+++ launch.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-#  ______                           __    
-# /_  __/___  ____ ___  _________ _/ /_   
-#  / / / __ \/ __ `__ \/ ___/ __ `/ __/   
-# / / / /_/ / / / / / / /__/ /_/ / /_     
-#/_/  \____/_/ /_/ /_/\___/\__,_/\__/     
+#  ______                           __
+# /_  __/___  ____ ___  _________ _/ /_
+#  / / / __ \/ __ `__ \/ ___/ __ `/ __/
+# / / / /_/ / / / / / / /__/ /_/ / /_
+#/_/  \____/_/ /_/ /_/\___/\__,_/\__/
 
 CATALINA_OPTS="-Xms256m -Xmx2G"
 CATALINA_OPTS="$CATALINA_OPTS"
@@ -24,6 +24,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 JAR_FILE="replace me"
 
+# http://docs.oracle.com/middleware/1213/wls/WLSRN/issues.htm#BCFFDBEB
 java -Djava.security.egd=file:/dev/./urandom \
 $CATALINA_OPTS -jar $DIR/$JAR_FILE \
 --server.port=5000 --server.tomcat.uri-encoding=UTF-8
